a5a959f37f2181849c4a84ef0243c21b699b7360
Noonmoon <tristandeering@hotmail.com>
2019-02-28 02:58:09 -0700

    todo list component created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
 
 export default {
   name: 'app',
   components: {
-
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: true
+        }
+      ]
+    }
   }
 }
 </script>
